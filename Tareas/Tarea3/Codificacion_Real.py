8bcf951b43cce03d01803fd562ca85f36141ce30
DanielRM23 <DanielRM@ciencias.unam.mx>
2025-09-15 15:02:00 -0600

    Codificación Real: ciclo general del algoritmo

--- Tareas/Tarea3/Codificacion_Real.py
+++ Tareas/Tarea3/Codificacion_Real.py
@@ -135,7 +135,6 @@ def mutacion(instancia, hijo, m):
             nuevo_gen = gen + s*rango*delta
 
             nuevo_gen = np.clip(nuevo_gen, low, high)
-
             hijo_mutado.append(nuevo_gen) 
         else:
             hijo_mutado.append(gen)
@@ -145,6 +144,77 @@ def mutacion(instancia, hijo, m):
 
 
 
+def ciclo_algoritmo(instancia, e, m):
+    """
+    Ejecuta el GA con elitismo de tamaño e y mutación tipo 'non-uniform' por bits (m).
+    Devuelve: (best_x, best_f, historial_best)
+    """
+    decimales = instancia["precision"]
+    M = instancia["pob_size"]
+    G = instancia["num_gen"]
+    p_cruce = instancia["p_cruce"]
+    f = instancia["f"]
+
+    # 1) Población inicial
+    poblacion = generar_poblacion(instancia)  # (M, n)
+
+    historial_best = []
+
+    for _ in range(G):
+        # 2) Evaluación
+        n = len(instancia["variables_rango"])
+        if n == 2:
+            fitness = evaluar_fitness_2D(instancia, poblacion)
+        else:
+            fitness = evaluar_fitness_5D(instancia, poblacion)
+
+        # 3) Elitismo
+        idx_orden = np.argsort(fitness)           # menor = mejor
+        idx_elite = idx_orden[:e]
+        elite      = poblacion[idx_elite].copy()
+        elite_fit  = fitness[idx_elite].copy()
+
+        # registrar mejor
+        historial_best.append(elite_fit[0])
+
+        # 4) Reproducción hasta completar M - e hijos
+        hijos = []
+        while len(hijos) < M - e:
+            padre = torneo(poblacion, fitness, k=5)
+            madre = torneo(poblacion, fitness, k=5)
+
+            if np.random.rand() < p_cruce:
+                h1, h2 = cruza(instancia, padre, madre)
+            else:
+                h1, h2 = padre.copy(), madre.copy()
+
+            h1 = mutacion(instancia, h1, m)
+            h2 = mutacion(instancia, h2, m)
+
+            # redondeo a la precisión deseada
+            h1 = np.round(h1, decimals=decimales)
+            h2 = np.round(h2, decimals=decimales)
+
+            hijos.append(h1)
+            if len(hijos) < M - e:
+                hijos.append(h2)
+
+        hijos = np.array(hijos, dtype=float)
+
+        # 5) Nueva generación = élite + hijos
+        poblacion = np.vstack([elite, hijos])
+
+    # evaluación final
+    n = len(instancia["variables_rango"])
+    if n == 2:
+        fitness = evaluar_fitness_2D(instancia, poblacion)
+    else:
+        fitness = evaluar_fitness_5D(instancia, poblacion)
+
+    best_idx = np.argmin(fitness)
+    best_x   = poblacion[best_idx]
+    best_f   = fitness[best_idx]
+    return best_x, best_f, np.array(historial_best)
 
 
 
@@ -156,21 +226,25 @@ def mutacion(instancia, hijo, m):
 # print(poblacion_evaluada_eggholder)
 
 
-esfera = INSTANCIAS["ESFERA 5-D"]
-poblacion_esfera = generar_poblacion(esfera)
-poblacion_evaluada_esfera = evaluar_fitness_5D(esfera, poblacion_esfera)
-#print(poblacion_evaluada_esfera)
+# esfera = INSTANCIAS["ESFERA 5-D"]
+# poblacion_esfera = generar_poblacion(esfera)
+# poblacion_evaluada_esfera = evaluar_fitness_5D(esfera, poblacion_esfera)
+# #print(poblacion_evaluada_esfera)
 
 
-padre = torneo(poblacion_esfera, poblacion_evaluada_esfera, k=5)
-madre = torneo(poblacion_esfera, poblacion_evaluada_esfera, k=5)
+# padre = torneo(poblacion_esfera, poblacion_evaluada_esfera, k=5)
+# madre = torneo(poblacion_esfera, poblacion_evaluada_esfera, k=5)
 
 
-hijo = cruza(esfera, padre, madre)
+# hijo = cruza(esfera, padre, madre)
 
-hijo_mutado = mutacion(esfera, hijo, m=4)
-print(hijo)
+# hijo_mutado = mutacion(esfera, hijo, m=4)
+# print(hijo)
 
-print(hijo_mutado)
+# print(hijo_mutado)
 
+inst = INSTANCIAS["ESFERA 5-D"]
+best_x, best_f, hist = ciclo_algoritmo(instancia=inst, e=4, m=5)
+print("Mejor x:", best_x, "\nMejor f:", best_f)
+plt.plot(hist); plt.xlabel("Generación"); plt.ylabel("Mejor fitness"); plt.show()
 
